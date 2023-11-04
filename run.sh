f1b24a66582d9f450f6971d29aeefe3589b5df88
Nelson Chen <crazysim@gmail.com>
2023-11-04 15:13:47 -0700

    good enough without math

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ ffmpeg -y \
 -i ecamera.hevc \
 -hwaccel auto \
 -i fcamera.hevc \
--filter_complex "[1:v]scale=iw/2:ih/2[front];[0:v][front]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
+-filter_complex "[1:v]scale=iw/4.5:ih/4.5,format=yuva420p,colorchannelmixer=aa=1[front];[0:v][front]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
 -t 10 \
 -c:v h264_videotoolbox \
 -b:v 10000k \
